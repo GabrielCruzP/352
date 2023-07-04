ef8729a1fda45edc3e631240df1b8fa2292d5739
GabrielCruzP <133056390+GabrielCruzP@users.noreply.github.com>
2023-07-04 14:28:12 -0600

    Se agrega SwitchCalificacion

---
+++ UNIDAD 2/2-1.-SwitchCalificacion/src/SwitchCalificacionJava.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Scanner;
+
+/**
+ *
+ *
+ */
+public class SwitchCalificacionJava {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+     public static void main(String[] args)throws IOException{
+
+                //definir variables
+                Scanner entrada = new Scanner(System.in);
+        int calificacion;
+        
+        //datos de entrada 
+        System.out.println("introduzca la calificacion:");
+        calificacion = entrada.nextInt();
+        //proceso (operación matemática)
+        //datos salida
+        switch (calificacion){
+            case 0 -> System.out.println ("MUY MAL");
+            case 1, 2, 3, 4, 5 -> System.out.println("INSUFICIENTE");
+            case 6 -> System.out.println("SUFICIENTE");
+            case 7, 8 -> System.out.println("BIEN");
+            case 9 -> System.out.println("NOTABLE");
+            case 10 -> System.out.println("SOBRESALIENTE");
+            default -> System.out.println("EEROR DE CAIFICACIÓN");
+        }
+
+    }
+
+}
